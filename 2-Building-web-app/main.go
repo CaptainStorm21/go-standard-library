bb6d8f52588543f23c26490cb90ce6e6ba0ae746
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-01-06 17:16:09 -0800

    23 - About is the about page handlers

--- 2-Building-web-app/main.go
+++ 2-Building-web-app/main.go
@@ -5,14 +5,39 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+const portNumber = ":8080"
 
-	_ = http.ListenAndServe(":8080", nil)
+// Home is the about page handlers
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this is a homepage")
+}
+
+// About is the about page handlers
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValue(2, 3)
+	fmt.Fprintf(w, "this is About page")
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is about page %d", sum))
+}
+
+// Adding values
+func addValue(x, y int) int {
+	return x + y
+}
+
+// Main is the application function
+func main() {
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	n, err := fmt.Fprintf(w, "Hello, world")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	// })
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting the server on %s", portNumber))
+
+	_ = http.ListenAndServe(portNumber, nil)
 }
