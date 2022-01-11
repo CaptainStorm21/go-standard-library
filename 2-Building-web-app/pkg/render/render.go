4d2ece04dd27a4b3e85d48055b3ae2e28812491f
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-01-10 21:27:08 -0800

    Lecture 29

--- 2-Building-web-app/pkg/render/render.go
+++ 2-Building-web-app/pkg/render/render.go
@@ -1,8 +1,10 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 )
@@ -10,21 +12,27 @@ import (
 var functions = template.FuncMap{}
 
 func RenderTemplate(w http.ResponseWriter, html string) {
-
-	_, err := RenderTemplateTest()
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("Error exists: ", err)
+		log.Fatal(err)
 	}
 
-	parsedTemplate, _ := template.ParseFiles("./templates/" + html)
-	err = parsedTemplate.Execute(w, nil)
+	t, ok := tc[html]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new (bytes.Buffer)
+	_ = t.Execute(buf, nil)
+	
+	_, err = buf.WriteTo(w)
 	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
+		fmt.Println("error writing template", err)
 	}
 }
 
-func RenderTemplateTest() (map[string]*template.Template, error) {
+// CreateTemplateCache() creates template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./templates/*page.html")
@@ -34,7 +42,7 @@ func RenderTemplateTest() (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		fmt.Println("rendering ", page)
+		// fmt.Println("rendering ", page)
 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return myCache, err
