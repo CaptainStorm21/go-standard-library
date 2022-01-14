6449482413dc3c4adca856f05c7c4c3d2c774491
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-01-13 17:38:10 -0800

    35. Sharing data with templates

--- 2-Building-web-app/pkg/handlers/handlers.go
+++ 2-Building-web-app/pkg/handlers/handlers.go
@@ -1,34 +1,47 @@
 package handlers
 
 import (
-	"net/http"
+	"github.com/captainstorm21/go-web-app/pkg/config"
+	"github.com/captainstorm21/go-web-app/pkg/models"
 	"github.com/captainstorm21/go-web-app/pkg/render"
-		"github.com/captainstorm21/go-web-app/pkg/config"
+	"net/http"
 )
 
+
+
+// Repo the repository used by the handlers
 var Repo *Repository
 
+// Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
 }
 
-func NewRepo(a *config.AppConfig) *Repository{
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
 	return &Repository{
-		App: a, 
+		App: a,
 	}
 }
 
-func NewHandlers (r *Repository){
-	Repo = r 
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
 }
-
 
 // Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 // About is the handler for the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	// send data to the template
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
